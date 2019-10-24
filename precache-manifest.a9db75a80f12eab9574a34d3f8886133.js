5d511314c1942124227d4f1d682343a5e9c87cea
Daym3l <daymel537@gmail.com>
2019-10-24 16:03:55 -0400

    Updates

---
+++ precache-manifest.a9db75a80f12eab9574a34d3f8886133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c923549a4a8044d5c372f55397ebe348",
+    "url": "/maniak/index.html"
+  },
+  {
+    "revision": "b8ce087edcdb3a67aa36",
+    "url": "/maniak/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "007333cc5ad7cac4d22b",
+    "url": "/maniak/static/css/main.1309eb5b.chunk.css"
+  },
+  {
+    "revision": "b8ce087edcdb3a67aa36",
+    "url": "/maniak/static/js/2.51bd6084.chunk.js"
+  },
+  {
+    "revision": "007333cc5ad7cac4d22b",
+    "url": "/maniak/static/js/main.f5b4fd2f.chunk.js"
+  },
+  {
+    "revision": "baef5bb4c072b494c5f4",
+    "url": "/maniak/static/js/runtime~main.d458c30b.js"
+  }
+]);
